9a44f162b65c70db74174cacdf1f03290859ed28
umang-888 <umang-888@users.noreply.github.com>
2026-01-14 11:30:00 +0530

    feat: Add auth barrel export

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,3 @@
+export { hashPassword, verifyPassword } from "./password";
+export { signToken, verifyToken, type JWTPayload } from "./jwt";
+export { setAuthCookie, removeAuthCookie, getAuthToken, getCurrentUser } from "./session";
